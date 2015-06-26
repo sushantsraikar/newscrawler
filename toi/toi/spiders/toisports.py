d25039b95bc649e84a3445f363b9b7fb7284c5f6
= <sushant.raikar@housing.com>
2015-06-26 19:04:26 +0530

    yaay working

--- toi/toi/spiders/toisports.py
+++ toi/toi/spiders/toisports.py
@@ -6,22 +6,36 @@ class ToiSpider(scrapy.Spider):
     name = "tos"
     allowed_domains = ['indiatimes.com']
     start_urls = [
-        "http://timesofindia.indiatimes.com/sports/"
+        "http://timesofindia.indiatimes.com/entertainment"
+
         ]
     links = []
-    links_valid = []
-        
+    
+    
+    outdir = '' 
     #typical url 'http://timesofindia.indiatimes.com/tech/tech-news/title/articleshow/47827034.cms'
     def parse_callback(self, response):
         #self.open_links(response)
 
         if response.url.split('/')[-2] == 'articleshow':
-            self.links_valid.append(response.url)
-            logging.warning("----->"+response.url)
-
-
+            #all the valid posts
+            title = response.url.split("/")[-3]
+            filename = self.outdir + '/' + title + '.html'
+            logging.warning("\n\n---------->"+filename)
+            try:
+                title =  response.xpath('//span[@class = "arttle"]/h1/text()').extract()[0]
+                image = response.xpath('//div[@class = "mainimg1"]//img/@src').extract()[0]
+                content = "\n".join(response.xpath('//div[@class = "Normal"]/text()').extract()) 
+            except:
+                return
+            with open(filename, 'w') as f:
+                f.write(title +"\n\n"+ image+"\n\n"+content)
 
     def parse(self, response):
+        self.outdir = 'crawled/' + response.url.split("/")[-1]
+        if not os.path.exists( self.outdir):
+            os.makedirs(self.outdir)
+
         for href in response.xpath('//@href').extract():
             url = href
             if 'http' not in url:
