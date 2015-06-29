9ac25c41ea221f8da9cd2db287501f450d8f1e02
= <sushant.raikar@housing.com>
2015-06-29 15:09:03 +0530

    command line args fixed

--- toi/toi/spiders/toiupload.py
+++ toi/toi/spiders/toiupload.py
@@ -11,12 +11,16 @@ class ToiSpider(scrapy.Spider):
 
         ]
     links = []
+    cat = 'tech'
     
     def __init__(self, *args, **kwargs):
         super(ToiSpider, self).__init__(*args, **kwargs) 
-        links = [kwargs.get('start_url')]
+        links = [kwargs.get('cat')]
         if links[0] :
-            self.start_urls = links  
+            tmp = self.start_urls[0]
+            self.cat = links[0]
+            self.start_urls[0] = tmp[:tmp.rfind('/')+1] +self.cat
+            
     
     outdir = '' 
     #typical url 'http://timesofindia.indiatimes.com/tech/tech-news/title/articleshow/47827034.cms'
@@ -31,14 +35,14 @@ class ToiSpider(scrapy.Spider):
             try:
                 title =  response.xpath('//span[@class = "arttle"]/h1/text()').extract()[0]
                 image = response.xpath('//div[@class = "mainimg1"]//img/@src').extract()[0]
-                content = "\n".join(response.xpath('//div[@class = "Normal"]/text()').extract()) 
+                content = "<br>".join(response.xpath('//div[@class = "Normal"]/text()').extract()) 
             except:
                 return
             with open(filename, 'w') as f:
                 f.write(title +"\n\n"+ image+"\n\n"+content)
-            payload = {'category':'tech','title':title,'content':content,'url':image}
+            payload = {'category':self.cat,'title':title,'content':content,'url':image}
             try:
-                requests.post('http://172.16.30.30:3000/news/create',data=payload)
+                requests.post('http://10.1.22.44:3000/news/create',data=payload)
             except:
                 print 'Server not live'
 
